d7df49e380d8476c1140c2538ed7b3ba95bc8d90
Juan Camilo Liberato <jcliberatol@unal.edu.co>
2014-05-23 19:06:21 -0500

    Initial Testing commit

---
+++ SICS/src/SICS.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : SICS.cpp
+// Author      : Juan Liberato
+// Version     :
+// Copyright   : Undefined
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
