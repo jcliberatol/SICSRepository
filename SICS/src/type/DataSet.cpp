7f8aa7bfdab949f45567086eee0f99f1481fa0b1
jlgpisa <jlgpisa@localhost.localdomain>
2014-06-20 22:41:12 -0500

    Description: Methods from old architecture are deleted (They are not necessary any more).

--- SICS/src/type/DataSet.cpp
+++ SICS/src/type/DataSet.cpp
@@ -7,32 +7,16 @@
 
 #include <type/DataSet.h>
 
-DataSet::DataSet( PatternMatrix *pM ) {
-
-	dataSet = pM;
-	datasetType = PATTERN_MATRIX;
+DataSet::DataSet(  ) {
+	// TODO Auto-generated constructor stub
 
 }
 
-DatasetType DataSet::getDatasetType() const {
-	return datasetType;
-}
-
-void DataSet::setDatasetType(DatasetType datasetType) {
-	this->datasetType = datasetType;
-}
-
-std::vector<int> DataSet::getDimensions() {
-	std::vector<int> dimensions;
-
-	//Number of items
-	dimensions.push_back(dataSet->getBitsetLength());
-	//TODO: Number of individuals: dimensions.push_back();
-
-	return dimensions;
+vector<int> DataSet::countItems() {
 }
 
 DataSet::~DataSet() {
 	// TODO Auto-generated destructor stub
 }
+
 
--- SICS/src/type/DataSet.h
+++ SICS/src/type/DataSet.h
@@ -13,19 +13,16 @@
 
 using namespace std;
 
-enum DatasetType { PATTERN_MATRIX };
-
 class DataSet {
 private:
-	DatasetType datasetType;;
-	PatternMatrix *dataSet;
 public:
-	DataSet ( PatternMatrix* );
-	DatasetType getDatasetType() const;
-	void setDatasetType(DatasetType datasetType);
-	vector<int> getDimensions();
-	//Dataset must define methods that allow the initial values algorithms to easily access to parameters they may need about the dataset,
-	//Size of the dataset, total sum of the patterns , sum of a specific column, accessors to specific columns or rows, etc.
+	// Constructor
+	DataSet ( );
+
+	// Methods
+	vector<int> countItems ();
+
+	// Destructor
 	virtual ~DataSet();
 
 };
