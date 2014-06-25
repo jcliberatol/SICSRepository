7b5d6e802fe51aff671d52e26592727b2c51cd32
jlgpisa <jlgpisa@localhost.localdomain>
2014-06-24 20:22:14 -0500

    Description: Implementation of function buildParameterSet for the 3PL model with Dichotomous and Unidimensional Models.

--- SICS/src/model/parameter/ThreePLModel.cpp
+++ SICS/src/model/parameter/ThreePLModel.cpp
@@ -14,9 +14,37 @@ ThreePLModel::ThreePLModel() {
 
 void ThreePLModel::buildParameterSet(ItemModel* itemModel,
 		DimensionModel* dimensionModel) {
+
+	if ( typeid(*itemModel) == typeid(DichotomousModel) ) {
+
+		if ( typeid(*dimensionModel) == typeid (UnidimensionalModel) ) {
+
+			int items = itemModel->countItems();
+
+			parameterSet[a] = new Matrix<double> (1,items);
+			parameterSet[d] = new Matrix<double> (1,items);
+			parameterSet[c] = new Matrix<double> (1,items);
+
+		}
+
+		else if ( typeid(*dimensionModel) == typeid (MultidimensionalModel) ) {
+			// TODO: Dichotomous Multidimensional
+		}
+
+		else if ( typeid(*dimensionModel) == typeid (MultiUniDimModel) ) {
+			// TODO: Dichotomous MultiUniDimensional
+		}
+
+	}
+
+	else if ( typeid(*dimensionModel) == typeid(PolytomousModel) ) {
+		// TODO: Polytomous Model for Unidimensional, Multidimensional and MultiUni
+	}
+
 }
 
-void ThreePLModel::setInitialPars(map<Parameter, Matrix<double> >* parameterSet) {
+void ThreePLModel::setInitialPars(
+	map<Parameter, Matrix<double> >* parameterSet) {
 }
 
 void ThreePLModel::calculateInitialPars() {
@@ -25,15 +53,15 @@ void ThreePLModel::calculateInitialPars() {
 void ThreePLModel::successProbability() {
 }
 
-const map<Parameter, Matrix<double> >* ThreePLModel::getParameterSet() const {
-	return parameterSet;
+const map<Parameter, Matrix<double> *>& ThreePLModel::getParameterSet() const {
+return this->parameterSet;
 }
 
-void ThreePLModel::setParameterSet(map<Parameter, Matrix<double> >* pair) {
+void ThreePLModel::setParameterSet(const map<Parameter, Matrix<double> *>& parameterSet) {
 	this->parameterSet = parameterSet;
 }
 
 ThreePLModel::~ThreePLModel() {
-	// TODO Auto-generated destructor stub
+// TODO Auto-generated destructor stub
 }
 
--- SICS/src/model/parameter/ThreePLModel.h
+++ SICS/src/model/parameter/ThreePLModel.h
@@ -8,23 +8,30 @@
 #ifndef THREEPLMODEL_H_
 #define THREEPLMODEL_H_
 
+#include <typeinfo>
 #include <model/parameter/ParameterModel.h>
+#include <model/item/DichotomousModel.h>
+#include <model/item/PolytomousModel.h>
+#include <model/dimension/UnidimensionalModel.h>
+#include <model/dimension/MultidimensionalModel.h>
+#include <model/dimension/MultiUniDimModel.h>
 
-class ThreePLModel : public ParameterModel {
+class ThreePLModel: public ParameterModel {
 public:
 	// Constructor
 	ThreePLModel();
 
 	// Methods
-	void buildParameterSet ( ItemModel *, DimensionModel * );
-	void setInitialPars ( map <Parameter, Matrix<double> > * );
-	void calculateInitialPars ();
-	void successProbability ();
+	void buildParameterSet(ItemModel *, DimensionModel *);
+	void setInitialPars(map<Parameter, Matrix<double> > *);
+	void calculateInitialPars();
+	void successProbability();
 
 	// Getters and Setters
-	const map<Parameter, Matrix<double> >* getParameterSet() const;
-	void setParameterSet(map<Parameter, Matrix<double> >*);
+	const map<Parameter, Matrix<double> *>& getParameterSet() const;
+	void setParameterSet(const map<Parameter, Matrix<double> *>&);
 
+	// Destructor
 	virtual ~ThreePLModel();
 };
 
