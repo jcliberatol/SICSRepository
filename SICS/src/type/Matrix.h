62e1be1bd40534e3f894b872ddc8f3376b413926
jlgpisa <jlgpisa@localhost.localdomain>
2014-06-27 11:29:00 -0500

    Description: Format changes.

--- SICS/src/type/Matrix.h
+++ SICS/src/type/Matrix.h
@@ -10,42 +10,41 @@
 #include <iostream>
 using namespace std;
 
-template <typename T>
+template<typename T>
 class Matrix;
 
-template <typename T>
-ostream& operator<< (ostream &, Matrix<T> &);
+template<typename T>
+ostream& operator<<(ostream &, Matrix<T> &);
 
-template <class T>
+template<class T>
 class Matrix {
 private:
 	int nCol = 0;
 	int nRow = 0;
 	T *memory;
 public:
-	Matrix();//Empty object
-	Matrix(int,int);//Two dimensional Matrix Constructor allocates memory
-	Matrix(Matrix&);//Copy constructor
+	Matrix(); //Empty object
+	Matrix(int, int); //Two dimensional Matrix Constructor allocates memory
+	Matrix(Matrix&); //Copy constructor
 	int nR(); // Returns number of rows
 	int nC(); //Returns number of columns
 	T & operator()(const int nCol, const int nRow); //Accessing operator for a element
-	friend ostream& operator<< <T>(ostream &, Matrix<T> &);//Output operator
-
+	friend ostream& operator<<<T>(ostream &, Matrix<T> &); //Output operator
 
 	virtual ~Matrix();
 };
 
-template <class T>
-int Matrix<T>::nR(){
+template<class T>
+int Matrix<T>::nR() {
 	return nRow;
 }
 
-template <class T>
-int Matrix<T>::nC(){
+template<class T>
+int Matrix<T>::nC() {
 	return nCol;
 }
 
-template <class T>
+template<class T>
 Matrix<T>::Matrix() {
 	// TODO Auto-generated constructor stub
 	nCol = 0;
@@ -53,34 +52,32 @@ Matrix<T>::Matrix() {
 	memory = NULL;
 }
 
-template <class T>
-Matrix<T>::Matrix(int r, int c){
+template<class T>
+Matrix<T>::Matrix(int r, int c) {
 	nCol = c;
 	nRow = r;
-	memory = new T [c*r];
+	memory = new T[c * r];
 }
-template <class T>
-T & Matrix<T>::operator()(const int c, const int r)
-{
-    return memory[nCol*r+c];
+template<class T>
+T & Matrix<T>::operator()(const int c, const int r) {
+	return memory[nCol * r + c];
 }
-template <class T>
+template<class T>
 Matrix<T>::~Matrix() {
 	// TODO Auto-generated destructor stub
 	delete[] memory;
 }
-template <class T>
-ostream& operator<< (ostream &out, Matrix<T> &M)
-{
-    // Since operator<< is a friend of the Point class, we can access
-    // Point's members directly.
-    for (int i = 0; i < M.nR(); ++i) {
-		for (int j = 0 ; j < M.nC(); j++){
-			out<<M(i,j)<<" ";
+template<class T>
+ostream& operator<<(ostream &out, Matrix<T> &M) {
+	// Since operator<< is a friend of the Point class, we can access
+	// Point's members directly.
+	for (int i = 0; i < M.nR(); ++i) {
+		for (int j = 0; j < M.nC(); j++) {
+			out << M(i, j) << " ";
 		}
-		out<<endl;
+		out << endl;
 	}
-    return out;
+	return out;
 }
 
 #endif /* MATRIX_H_ */
