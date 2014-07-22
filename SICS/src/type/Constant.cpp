4af0fee881635b0c0a80977d693f5dcce7e11e57
jlgpisa <jlgpisa@localhost.localdomain>
2014-07-21 21:49:34 -0500

    Description: Constant class is created (according to architecture 3.0).

---
+++ SICS/src/type/Constant.cpp
@@ -0,0 +1,14 @@
+/*
+ * Constant.cpp
+ *
+ *  Created on: 21 Jul 2014
+ *      Author: jlgpisa
+ */
+
+#include <type/Constant.h>
+
+double Constant::NORM_CONST = 1;
+double Constant::MAX_EXP = 35.0;
+double Constant::INFINITE = 1e30;
+double Constant::EPSILON = 1e-20;
+string Constant::INITIAL_VALUE_METHOD = "ANDRADE";
---
+++ SICS/src/type/Constant.h
@@ -0,0 +1,24 @@
+/*
+ * Constant.h
+ *
+ *  Created on: 21 Jul 2014
+ *      Author: jlgpisa
+ */
+
+#ifndef CONSTANT_H_
+#define CONSTANT_H_
+
+#include <string>
+
+using namespace std;
+
+class Constant {
+public:
+	static double NORM_CONST;
+	static double MAX_EXP;
+	static double INFINITE;
+	static double EPSILON;
+	static string INITIAL_VALUE_METHOD;
+};
+
+#endif /* CONSTANT_H_ */
