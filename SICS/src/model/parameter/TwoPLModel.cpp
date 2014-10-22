9e85ab4f2d64052ea0b3918906c7e3239cb57b86
Cristian <cesandovalp@unal.edu.co>
2014-10-22 13:28:41 -0500

    Added successProabability function
    1/[ 1 + e^( -D * ( a*theta - a*b ) ) ]

--- SICS/src/model/parameter/TwoPLModel.cpp
+++ SICS/src/model/parameter/TwoPLModel.cpp
@@ -22,12 +22,31 @@ map<Parameter, Matrix<double> *> TwoPLModel::getParameterSet()  {
 	return (this->parameterSet);
 }
 
+double TwoPLModel::successProbability(double theta, double a, double b, double d) {
+	long double exponential = (Constant::D_CONST*((a*theta)-(a*b)));
+	if ( exponential > Constant::MAX_EXP ) {
+		exponential = Constant::MAX_EXP;
+	}
+	
+	else if ( exponential < -(Constant::MAX_EXP*1.0) ) {
+		exponential = -Constant::MAX_EXP;
+	}
+	
+	exponential = exp(-exponential);
+	
+	return ( 1 / ( 1 + exponential ) );
+}
+
 void TwoPLModel::setParameterSet(map<Parameter, Matrix<double> *> pair) {
 	this->parameterSet = parameterSet;
 }
 
 double TwoPLModel::getProbability(int node, int item) {
-	return (0);
+	return ((*probabilityMatrix)(node, item));
+}
+
+map<Parameter, Matrix<double> *> TwoPLModel::getParameterSet() {
+	return (this->parameterSet);
 }
 
 TwoPLModel::~TwoPLModel() {
