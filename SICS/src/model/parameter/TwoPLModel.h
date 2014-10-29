882d6c9b243d5b9126a391cebe2776018cead011
AkumaHikki <cesandovalp@unal.edu.co>
2014-10-29 03:12:51 -0500

    Added functions to calculate gradient and hessian

--- SICS/src/model/parameter/TwoPLModel.h
+++ SICS/src/model/parameter/TwoPLModel.h
@@ -13,6 +13,10 @@
 #include <cmath>
 #include <typeinfo>
 #include <model/dimension/UnidimensionalModel.h>
+#include <model/dimension/MultidimensionalModel.h>
+#include <model/dimension/MultiUniDimModel.h>
+#include <model/item/PolytomousModel.h>
+#include <model/item/DichotomousModel.h>
 
 class TwoPLModel: public ParameterModel {
 public:
@@ -22,13 +26,26 @@ public:
 	// Methods
 	void buildParameterSet(ItemModel *, DimensionModel *);
 	void successProbability(DimensionModel *, QuadratureNodes *);
-	static double successProbability(double, double, double, double);
+	static double successProbability(double, double, double);
 	static double logLikelihood(double*, double*, int, int);
+	static double patternProbability();
+	static void gradientAux(long double tp, long double tq,
+			long double * grandient);
+	static void gradient(double*, double*, int, int, double*);
+	static void Ngradient(double* args, double* pars, int nargs, int npars,
+			double* gradient);
+	static void Hessian(double* args, double* pars, int nargs, int npars,
+			double* Hessian);
+	static void NHessian(double* args, double* pars, int nargs, int npars,
+			double* Hessian);
+	static void itemHessian(double* args, double* pars, int nargs, int npars,
+			double* Hessian);
+	static void itemgradient(double*, double*, int, int, double*);
 
 	// Getters and Setters
-	map<Parameter, Matrix<double> *> getParameterSet() ;
+	map<Parameter, Matrix<double> *> getParameterSet();
 	void setParameterSet(map<Parameter, Matrix<double> *>);
-	double getProbability (int, int);
+	double getProbability(int, int);
 
 	// Destructor
 	virtual ~TwoPLModel();
