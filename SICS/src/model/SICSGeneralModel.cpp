dbcb20d389b94cc2be1c0a421af673e9babbd7c4
anmrdz <anmrdz@users.noreply.github.com>
2014-11-21 03:25:31 -0500

    Update SICSGeneralModel.cpp

--- SICS/src/model/SICSGeneralModel.cpp
+++ SICS/src/model/SICSGeneralModel.cpp
@@ -12,23 +12,48 @@ SICSGeneralModel::SICSGeneralModel() {
 
 }
 
-ParameterModel* SICSGeneralModel::createParameterModel() {
-	//ParameterModel *parameterModel = new ThreePLModel();//TODO FIX FOR DECISION OF NEW MODELS
-	ParameterModel *parameterModel = new RaschModel();
+ParameterModel* SICSGeneralModel::createParameterModel(int model) {
+
+	if (model == Constant::THREE_PL) {
+		ParameterModel *parameterModel;
+		parameterModel = new ThreePLModel();
+		return (parameterModel);
+	}
+
+	if (model == Constant::TWO_PL) {
+		ParameterModel *parameterModel;
+		parameterModel = new TwoPLModel();
+		return (parameterModel);
+	}
+
+	if (model == Constant::RASCH_A1) {
+		ParameterModel *parameterModel;
+		// TODO parameterModel = new TwoPLModel();
+		return (parameterModel);
+	}
+
+	if (model == Constant::RASCH_A_CONSTANT) {
+		ParameterModel *parameterModel;
+		// TODO parameterModel = new RaschModel();
+		return (parameterModel);
+	}
+
+	// Default Parameter Model.
+	ParameterModel *parameterModel;
+	parameterModel = new ThreePLModel();
 	return (parameterModel);
 }
 
 ItemModel* SICSGeneralModel::createItemModel() {
-	ItemModel *itemModel = new DichotomousModel();//TODO FIX FOR DECISION OF NEW MODELS
+	ItemModel *itemModel = new DichotomousModel(); //TODO FIX FOR DECISION OF NEW MODELS
 	return (itemModel);
 }
 
 DimensionModel* SICSGeneralModel::createDimensionModel() {
-	DimensionModel *dimensionModel = new UnidimensionalModel();//TODO FIX FOR DECISION OF NEW MODELS
+	DimensionModel *dimensionModel = new UnidimensionalModel(); //TODO FIX FOR DECISION OF NEW MODELS
 	return (dimensionModel);
 }
 
 SICSGeneralModel::~SICSGeneralModel() {
 	// TODO Auto-generated destructor stub
 }
-
