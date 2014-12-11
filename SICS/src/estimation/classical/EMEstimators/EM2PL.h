11036a047f3a1d0309a599592c28d0afa757cf29
AkumaHikki <cesandovalp@unal.edu.co>
2014-12-11 17:17:21 +0000

    Improve the number of functions calls

--- SICS/src/estimation/classical/EMEstimators/EM2PL.h
+++ SICS/src/estimation/classical/EMEstimators/EM2PL.h
@@ -9,26 +9,29 @@
 #define EM2PL_H_
 #include <estimation/classical/EMEstimators/EMEstimator.h>
 #include <model/parameter/TwoPLModel.h>
-class EM2PL : public EMEstimator {
+class EM2PL: public EMEstimator {
 public:
-	EM2PL(){}
-	virtual ~EM2PL(){}
-
-	virtual void transform(Model* m){
-		for (int i = 0; i < m->getItemModel()->countItems(); ++i) {
-				double qa = (*m->getParameterModel()->getParameterSet()[a])(0, i);
-				double qb = (*m->getParameterModel()->getParameterSet()[d])(0, i);
-			}
+	EM2PL() {
+	}
+	virtual ~EM2PL() {
 	}
 
-	virtual void untransform(Model* m){
+	virtual void transform(Model* m) {
 		for (int i = 0; i < m->getItemModel()->countItems(); ++i) {
-				double qa = (*m->getParameterModel()->getParameterSet()[a])(0, i);
-				double qb = (*m->getParameterModel()->getParameterSet()[d])(0, i);
-			}
+			double qa = (*m->getParameterModel()->getParameterSet()[a])(0, i);
+			double qb = (*m->getParameterModel()->getParameterSet()[d])(0, i);
 		}
+	}
 
-	virtual void setInitialValues(map<Parameter, Matrix<double>*> parameterSet, Model* m) {
+	virtual void untransform(Model* m) {
+		for (int i = 0; i < m->getItemModel()->countItems(); ++i) {
+			double qa = (*m->getParameterModel()->getParameterSet()[a])(0, i);
+			double qb = (*m->getParameterModel()->getParameterSet()[d])(0, i);
+		}
+	}
+
+	virtual void setInitialValues(map<Parameter, Matrix<double>*> parameterSet,
+			Model* m) {
 		m->getParameterModel()->setParameterSet(parameterSet);
 	}
 
@@ -42,23 +45,26 @@ public:
 		 *
 		 * The default method is OSPINA
 		 */
-		if(!method == Constant::RANDOM){
+		if (!method == Constant::RANDOM) {
 			std::srand(std::time(0)); // use current time as seed for random generator
 			int items = m->getParameterModel()->getParameterSet()[a]->nC();
-			for (int i = 0 ; i < items ; i++){
-				(*m->getParameterModel()->getParameterSet()[a])(0, i)= randomd()*2;
+			for (int i = 0; i < items; i++) {
+				(*m->getParameterModel()->getParameterSet()[a])(0, i) =
+						randomd() * 2;
 				//fill b
-				(*m->getParameterModel()->getParameterSet()[d])(0, i)= randomd()*4-2 ;
+				(*m->getParameterModel()->getParameterSet()[d])(0, i) =
+						randomd() * 4 - 2;
 			}
 		}
 
-
 		//ANDRADE O( items * numberOfPattern )
 		if (method == Constant::ANDRADE) {
-			int items = m->getParameterModel()->getParameterSet()[d]->nC(), numeroDePatrones = 0 , iter, ifault;
+			int items = m->getParameterModel()->getParameterSet()[d]->nC(),
+					numeroDePatrones = 0, iter, ifault;
 			PatternMatrix* data =
 					dynamic_cast<PatternMatrix *>(m->getItemModel()->getDataset());
-			double Ni = data->countIndividuals(), PII, frequencyV, mT, mU, mTU, mUU, covar, sdU, sdT, corr, result;
+			double Ni = data->countIndividuals(), PII, frequencyV, mT, mU, mTU,
+					mUU, covar, sdU, sdT, corr, result;
 			for (data->resetIterator(); !data->checkEnd(); data->iterate())
 				numeroDePatrones++; // esto se debe poder hacer de una forma mas optima! en patternMatrix tener el tamaño!
 			double *T = new double[numeroDePatrones], *U =
@@ -82,8 +88,8 @@ public:
 					UU[iter] = U[iter] * U[iter];
 					mT += frequencyV * T[iter];
 					mU += frequencyV * U[iter];
-				    mTU += frequencyV * TU[iter];
-				    mUU += frequencyV * UU[iter];
+					mTU += frequencyV * TU[iter];
+					mUU += frequencyV * UU[iter];
 					iter++;
 				}
 				PII /= Ni;
@@ -95,7 +101,8 @@ public:
 				iter = 0;
 				sdT = 0.0;
 				sdU = 0.0;
-				for (data->resetIterator(); !data->checkEnd(); data->iterate()) {
+				for (data->resetIterator(); !data->checkEnd();
+						data->iterate()) {
 					frequencyV = data->getCurrentFrequency();
 					Tm[iter] = T[iter] - mT;
 					Um[iter] = U[iter] - mU;
@@ -106,243 +113,256 @@ public:
 				sdT = std::sqrt(sdT / (Ni - 1.0));
 				sdU = std::sqrt(sdU / (Ni - 1.0));
 				corr = covar / (sdT * sdU);
-				(*m->getParameterModel()->getParameterSet()[a])(0, i) = std::sqrt((corr * corr) / (1.0 - corr * corr));
-				(*m->getParameterModel()->getParameterSet()[d])(0, i) = -(ppnd(PII, &ifault)) / corr;
+				(*m->getParameterModel()->getParameterSet()[a])(0, i) =
+						std::sqrt((corr * corr) / (1.0 - corr * corr));
+				(*m->getParameterModel()->getParameterSet()[d])(0, i) = -(ppnd(
+						PII, &ifault)) / corr;
 			}
 		}
 	}
 
-	virtual void stepE(Model* m, Matrix<double>* f, Matrix<double>* r,  QuadratureNodes* nodes){
+	virtual void stepE(Model* m, Matrix<double>* f, Matrix<double>* r,
+			QuadratureNodes* nodes) {
 
 		/*
-			 * What we need
-			 * q
-			 * a pattern iterator
-			 * item number
-			 * success probability matrix
-			 * thetas
-			 * weights
-			 * parameter set
-			 */
-			//Dataset by patterns
-			PatternMatrix* data =
-					dynamic_cast<PatternMatrix *>(m->getItemModel()->getDataset());
-			//Pattern iterator is data->iterator
-			//Item number
-			const double items = data->countItems();
-			//Success probability matrix is obtained via pm->getProbability(int,int)
-			ParameterModel* pm = m->getParameterModel();
-			//Thetas
-			Matrix<double>* thetas = nodes->getTheta();
-			//Amount of nodes
-			const int q = nodes->size();
-			//Weights
-			Matrix<double>* weights = nodes->getWeight();
-			//A Matrix
-			Matrix<double>* A = m->getParameterModel()->getParameterSet()[a];
-			//B Matrix
-			Matrix<double>* B = m->getParameterModel()->getParameterSet()[d];
-			//C Matrix
-			//Matrix<double>* C = model->getParameterModel()->getParameterSet()[c];
-			//Auxiliar array for the nodes
-			long double faux[q];
-			long double sum = 0.0;
-			//Restart f and r to zero
-			f->reset();
-			r->reset();
-			//Calculates the success probability for all the nodes.
-			m->successProbability(nodes);
+		 * What we need
+		 * q
+		 * a pattern iterator
+		 * item number
+		 * success probability matrix
+		 * thetas
+		 * weights
+		 * parameter set
+		 */
+		//Dataset by patterns
+		PatternMatrix* data =
+				dynamic_cast<PatternMatrix *>(m->getItemModel()->getDataset());
+		//Pattern iterator is data->iterator
+		//Item number
+		const double items = data->countItems();
+		//Success probability matrix is obtained via pm->getProbability(int,int)
+		ParameterModel* pm = m->getParameterModel();
+		//Thetas
+		Matrix<double>* thetas = nodes->getTheta();
+		//Amount of nodes
+		const int q = nodes->size();
+		//Weights
+		Matrix<double>* weights = nodes->getWeight();
+		//A Matrix
+		Matrix<double>* A = m->getParameterModel()->getParameterSet()[a];
+		//B Matrix
+		Matrix<double>* B = m->getParameterModel()->getParameterSet()[d];
+		//C Matrix
+		//Matrix<double>* C = model->getParameterModel()->getParameterSet()[c];
+		//Auxiliar array for the nodes
+		long double faux[q];
+		long double sum = 0.0;
+		//Restart f and r to zero
+		f->reset();
+		r->reset();
+		//Calculates the success probability for all the nodes.
+		m->successProbability(nodes);
 
-			//TODO CAREFULLY PARALLELIZE FOR
-			int counter = 0;
-			for (data->resetIterator(); !data->checkEnd(); data->iterate()) {
-				counter++;
-				for (int k = 0; k < q; k++) {
-					faux[k] = 1;
-				}
-				//Calculate g*(k) for all the k's
-				//first calculate the P for each k and store it in the array f aux
-				for (int k = 0; k < q; k++) {
-					//Calculate the p (iterate over the items in the productory)
-					for (unsigned int i = 0; i < items; i++) {
-						double prob = pm->getProbability(k, i);
-						if (!data->getCurrentBitSet()[items - i - 1]) {
-							prob = 1 - prob;
-						}
-						faux[k] = faux[k] * prob;
-					}
-					//At this point the productory is calculated and faux[k] is equivalent to p(u_j,theta_k)
-					//Now multiply by the weight
-					faux[k] = faux[k] * (*weights)(0, k);
-				}
-				//compute the total of the p*a' (denominator of the g*)
-				sum = 0.0;
-				for (int k = 0; k < q; k++) {
-					sum += faux[k];
-				}
-				for (int k = 0; k < q; k++) {
-					faux[k] = faux[k] / sum; //This is g*_j_k
-					//Multiply the f to the frequency of the pattern
-					faux[k] = ((long double) data->getCurrentFrequency()) * faux[k];
-					(*f)(0, k) += faux[k];
-					//Now selectively add the faux to the r
-					for (unsigned int i = 0; i < items; i++) {
-						if (data->getCurrentBitSet()[items - i - 1]) {
-							(*r)(k, i) = (*r)(k, i) + faux[k];
-						} // if
-					} // for
-				} // for
+		//Used in for
+		unsigned int k;
+		unsigned int i;
+		double prob;
+
+		//TODO CAREFULLY PARALLELIZE FOR
+		int counter = 0;
+		for (data->resetIterator(); !data->checkEnd(); data->iterate()) {
+			boost::dynamic_bitset<> currentBitSet = data->getCurrentBitSet();
+			long double currentFrequency =
+					(long double) data->getCurrentFrequency();
+
+			counter++;
+
+			for (int k = 0; k < q; k++) {
+				faux[k] = 1;
 			}
+			//Calculate g*(k) for all the k's
+			//first calculate the P for each k and store it in the array f aux
+			for (k = 0; k < q; k++) {
+				//Calculate the p (iterate over the items in the productory)
+				for (i = 0; i < items; i++) {
+					prob = pm->getProbability(k, i);
+					if (!currentBitSet[items - i - 1]) {
+						prob = 1 - prob;
+					}
+					faux[k] = faux[k] * prob;
+				}
+				//At this point the productory is calculated and faux[k] is equivalent to p(u_j,theta_k)
+				//Now multiply by the weight
+				faux[k] = faux[k] * (*weights)(0, k);
+			}
+			//compute the total of the p*a' (denominator of the g*)
+			sum = 0.0;
+			for (k = 0; k < q; k++) {
+				sum += faux[k];
+			}
+			for (k = 0; k < q; k++) {
+				faux[k] = faux[k] / sum; //This is g*_j_k
+				//Multiply the f to the frequency of the pattern
+				faux[k] = currentFrequency * faux[k];
+				(*f)(0, k) += faux[k];
+				//Now selectively add the faux to the r
+				for (i = 0; i < items; i++) {
+					if (currentBitSet[items - i - 1]) {
+						(*r)(k, i) = (*r)(k, i) + faux[k];
+					} // if
+				} // for
+			} // for
+		}
 	}
 
-	virtual void stepM(Model* m, Matrix<double>* f, Matrix<double>* r,  QuadratureNodes* nodes){
+	virtual void stepM(Model* m, Matrix<double>* f, Matrix<double>* r,
+			QuadratureNodes* nodes) {
 		/*
-			 */
-			//Step M implementation using the BFGS Algorithm
-			/*
-			 * What we need
-			 * fptr the pointer to loglik
-			 * gprt the pointer to gradient
-			 * hessptr the pointer to hessian matrix calculatrix
-			 * args the a,b, and c
-			 * pars, the other parameters q and stuff
-			 * nargs, npars, sizes.
-			 */
-			//fptr
-			double (*fptr)(double*, double*, int, int);
-			void (*gptr)(double*, double*, int, int, double*);
-			void (*hptr)(double*, double*, int, int, double*);
-			fptr = &TwoPLModel::logLikelihood;
-			gptr = &TwoPLModel::gradient;
-			hptr = NULL;
-			//cout<<"Address : "<<&gptr<<" "<<&hptr<<endl;
-			int It = m->getItemModel()->getDataset()->countItems();
-			int q = nodes->size();
-			double args[3 * It];
-			double pars[2 + 2 * q + q * It];
-			int nargs = 3 * It;
-			int npars = 2 + 2 * q + q * It;
-			//filling args
-			int nA = 0;
-			// Obtain a
-			//A Matrix
-			Matrix<double>* A = m->getParameterModel()->getParameterSet()[a];
-			//B Matrix
-			Matrix<double>* B = m->getParameterModel()->getParameterSet()[d];
-			//C Matrix
-			//Matrix<double>* C = model->getParameterModel()->getParameterSet()[c];
+		 */
+		//Step M implementation using the BFGS Algorithm
+		/*
+		 * What we need
+		 * fptr the pointer to loglik
+		 * gprt the pointer to gradient
+		 * hessptr the pointer to hessian matrix calculatrix
+		 * args the a,b, and c
+		 * pars, the other parameters q and stuff
+		 * nargs, npars, sizes.
+		 */
+		//fptr
+		double (*fptr)(double*, double*, int, int);
+		void (*gptr)(double*, double*, int, int, double*);
+		void (*hptr)(double*, double*, int, int, double*);
+		fptr = &TwoPLModel::logLikelihood;
+		gptr = &TwoPLModel::gradient;
+		hptr = NULL;
+		//cout<<"Address : "<<&gptr<<" "<<&hptr<<endl;
+		int It = m->getItemModel()->getDataset()->countItems();
+		int q = nodes->size();
+		double args[3 * It];
+		double pars[2 + 2 * q + q * It];
+		int nargs = 3 * It;
+		int npars = 2 + 2 * q + q * It;
+		//filling args
+		int nA = 0;
+		// Obtain a
+		//A Matrix
+		Matrix<double>* A = m->getParameterModel()->getParameterSet()[a];
+		//B Matrix
+		Matrix<double>* B = m->getParameterModel()->getParameterSet()[d];
+		//C Matrix
+		//Matrix<double>* C = model->getParameterModel()->getParameterSet()[c];
 
-			Matrix<double> DA(*A);
-			Matrix<double> DB(*B);
-			//Matrix<double> DC(*C);
+		Matrix<double> DA(*A);
+		Matrix<double> DB(*B);
+		//Matrix<double> DC(*C);
 
-			for (int i = 0; i < It; i++) {
-				args[nA] = (*A)(0, i);
-				nA++;
-			}
+		for (int i = 0; i < It; i++) {
+			args[nA] = (*A)(0, i);
+			nA++;
+		}
 
-			// Obtain b
-			for (int i = 0; i < It; i++) {
-				args[nA] = (*B)(0, i);
-				nA++;
-			}
+		// Obtain b
+		for (int i = 0; i < It; i++) {
+			args[nA] = (*B)(0, i);
+			nA++;
+		}
 
-			//Filling pars
-			int nP = 0;
-			// Obtain q
-			pars[nP] = q;
+		//Filling pars
+		int nP = 0;
+		// Obtain q
+		pars[nP] = q;
+		nP++;
+		// Obtain I
+		pars[nP] = It;
+		nP++;
+		// Obtain theta
+		//Thetas
+
+		Matrix<double>* thetas = nodes->getTheta();
+		for (int k = 0; k < q; k++) {
+			pars[nP] = (*thetas)(0, k);	//TODO correct indexing on this and nearby matrices
 			nP++;
-			// Obtain I
-			pars[nP] = It;
+		}
+		// Obtain f
+		for (int k = 0; k < q; k++) {
+			pars[nP] = (*f)(0, k);
 			nP++;
-			// Obtain theta
-			//Thetas
-
-			Matrix<double>* thetas = nodes->getTheta();
-			for (int k = 0; k < q; k++) {
-				pars[nP] = (*thetas)(0, k);	//TODO correct indexing on this and nearby matrices
+		}
+		// Obtain r
+		for (int k = 0; k < q; k++) {
+			for (int i = 0; i < It; i++) {
+				pars[nP] = (*r)(k, i);
 				nP++;
 			}
-			// Obtain f
-			for (int k = 0; k < q; k++) {
-				pars[nP] = (*f)(0, k);
-				nP++;
-			}
-			// Obtain r
-			for (int k = 0; k < q; k++) {
-				for (int i = 0; i < It; i++) {
-					pars[nP] = (*r)(k, i);
-					nP++;
-				}
-			}
-			nargs = nA;
-			npars = nP;
-			/*
-			 * Chooses the method
-			 * method 1 is NR
-			 * method 2 is BFGS
-			 */
-			Optimizer* optim;
-			optim = new Optimizer();
-			optim->searchOptimal(fptr, gptr, hptr, args, pars, nargs, npars);
+		}
+		nargs = nA;
+		npars = nP;
+		/*
+		 * Chooses the method
+		 * method 1 is NR
+		 * method 2 is BFGS
+		 */
+		Optimizer* optim;
+		optim = new Optimizer();
+		optim->searchOptimal(fptr, gptr, hptr, args, pars, nargs, npars);
 
-			// Now pass the optimals to the Arrays.
+		// Now pass the optimals to the Arrays.
 
-			nA = 0;
-			// Obtain a
-			for (int i = 0; i < It; i++) {
-				(*A)(0, i) = args[nA++];
-				if (fabs((*A)(0, i)) > abs(10)) {
-					(*A)(0, i) = 0.851;
-		//			cout << "A reset." << endl;
-				}
-
-			}
-			// Obtain b
-			for (int i = 0; i < It; i++) {
-				(*B)(0, i) = args[nA++];
-				if (fabs((*B)(0, i)) > abs(-50)) {
-					(*B)(0, i) = 0.5;
-		//			cout << "B reset." << endl;
-				}
+		nA = 0;
+		// Obtain a
+		for (int i = 0; i < It; i++) {
+			(*A)(0, i) = args[nA++];
+			if (fabs((*A)(0, i)) > abs(10)) {
+				(*A)(0, i) = 0.851;
+				//			cout << "A reset." << endl;
 			}
 
-
-			//Boundary regularize the arguments
-			//B = 0.5;
-			//A = 0.851
-
-			//Obtain the deltas
-			//Perform substracts
-			double maxDelta = 0;
-			double meanDelta = 0;
-			int DeltaC = 0;
-			for (int v1 = 0; v1 < It; ++v1) {
-				DA(0, v1) = DA(0, v1) - (*A)(0, v1);
-				DB(0, v1) = DB(0, v1) - (*B)(0, v1);
-				meanDelta = +fabs(DA(0, v1));
-				meanDelta = +fabs(DB(0, v1));
-				DeltaC += 3;
-				if (fabs(DA(0, v1)) > maxDelta) {
-					maxDelta = fabs(DA(0, v1));
-				}
-				if (fabs(DB(0, v1)) > maxDelta) {
-					maxDelta = fabs(DB(0, v1));
-				}
+		}
+		// Obtain b
+		for (int i = 0; i < It; i++) {
+			(*B)(0, i) = args[nA++];
+			if (fabs((*B)(0, i)) > abs(-50)) {
+				(*B)(0, i) = 0.5;
+				//			cout << "B reset." << endl;
 			}
-			meanDelta = meanDelta / DeltaC;
-			if (meanDelta < 0.0001 and maxDelta < 0.001) {
-				m->itemParametersEstimated = true;
+		}
+
+		//Boundary regularize the arguments
+		//B = 0.5;
+		//A = 0.851
+
+		//Obtain the deltas
+		//Perform substracts
+		double maxDelta = 0;
+		double meanDelta = 0;
+		int DeltaC = 0;
+		for (int v1 = 0; v1 < It; ++v1) {
+			DA(0, v1) = DA(0, v1) - (*A)(0, v1);
+			DB(0, v1) = DB(0, v1) - (*B)(0, v1);
+			meanDelta = +fabs(DA(0, v1));
+			meanDelta = +fabs(DB(0, v1));
+			DeltaC += 3;
+			if (fabs(DA(0, v1)) > maxDelta) {
+				maxDelta = fabs(DA(0, v1));
 			}
-			//And set the parameter sets
-			map<Parameter, Matrix<double> *> parSet;
-			parSet[a] = A;
-			parSet[d] = B;
-			// llenar las tres matrices
-			m->getParameterModel()->setParameterSet(parSet);
+			if (fabs(DB(0, v1)) > maxDelta) {
+				maxDelta = fabs(DB(0, v1));
+			}
+		}
+		meanDelta = meanDelta / DeltaC;
+		if (meanDelta < 0.0001 and maxDelta < 0.001) {
+			m->itemParametersEstimated = true;
+		}
+		//And set the parameter sets
+		map<Parameter, Matrix<double> *> parSet;
+		parSet[a] = A;
+		parSet[d] = B;
+		// llenar las tres matrices
+		m->getParameterModel()->setParameterSet(parSet);
 
-
-};
+	}
+	;
 
 };
 #endif /* EM2PL_H_ */
