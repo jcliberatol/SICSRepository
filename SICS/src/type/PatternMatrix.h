54a8a5e73682a7fea6e1810832244c304debddf9
Cristian <cesandovalp@unal.edu.co>
2015-01-28 18:11:39 +0500

    Update PatternMatrix.h

--- SICS/src/type/PatternMatrix.h
+++ SICS/src/type/PatternMatrix.h
@@ -8,7 +8,6 @@
 #ifndef PATTERNMATRIX_H_
 #define PATTERNMATRIX_H_
 
-#include <boost/dynamic_bitset.hpp>
 #include <map>
 #include <iostream>
 #include <type/DataSet.h>
@@ -24,32 +23,27 @@ private:
 	
 public:
 	int size;
-	//map<boost::dynamic_bitset<>,int> matrix;
-	//map<bool*, int> matrix; //Not viable due to the fact that a container cant use a array as a key.
 	map<vector<char>, int> matrix;
+	int * count_set_bits;
 
 	//Constructor
 	PatternMatrix(int size);
 
 	// Methods
-	//std::map<boost::dynamic_bitset<>, int>::const_iterator iterator; /**use this when reading in order*/
 	std::map<vector<char>, int>::const_iterator iterator; /**use this when reading in order*/
 	inline void resetIterator(){iterator = matrix.begin();}
 	inline bool checkEnd(){return (iterator==matrix.end());}
 	inline void iterate(){++iterator;}
+	int countBitSet(bool * bitset, int index);
 	//inline void values(){cout<<iterator->first<<" values  "<<iterator->second<<endl;}
-	//inline boost::dynamic_bitset<> getCurrentBitSet(){return (iterator->first);}
 	inline vector<char> getCurrentBitSet(){return (iterator->first);}
 	inline long int getCurrentFrequency(){return (iterator->second);}
-//	void push(boost::dynamic_bitset<>);/**Use this to fill the pattern matrix*/
-//	void push(boost::dynamic_bitset<>,int);/**Use this to fill the pattern matrix many times with a pattern*/
 	void push(vector<char>);/**Use this to fill the pattern matrix*/
 	void push(vector<char>,int);/**Use this to fill the pattern matrix many times with a pattern*/
 	int freq(vector<char>);//Frequency
 	void flush();/**Use this to clean matrix*/
 	void print();
 
-	//int& operator()(boost::dynamic_bitset<>); /**Use this to access a specific pattern frecuency and modify it*/
 	friend std::ostream& operator<< (std::ostream &, PatternMatrix &);/**Output operator*/
 
 	//DataSet implementations
