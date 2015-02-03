595d4c7f742f995fc05184483f2b745dce6afd7c
akumahikki <cesandovalp@unal.edu.co>
2015-02-03 10:44:18 +0000

    Latent Traits update

---
+++ SICS/src/type/LatentTraits.h
@@ -0,0 +1,31 @@
+/*
+ * LatentTraits.h
+ *
+ *  Created on: Feb 2, 2015
+ *      Author: cristian
+ */
+
+#ifndef TYPE_LATENTTRAITS_H_
+#define TYPE_LATENTTRAITS_H_
+#include <type/PatternMatrix.h>
+
+class LatentTraits {
+public:
+	int dim;
+
+	LatentTraits(PatternMatrix * p, const int dims = 1){
+		pm = p;
+		int rows = pm->matrix.size();
+		traits = new Matrix<double>(rows, dims);
+		dim = dims;
+	}
+	virtual ~LatentTraits(){
+		delete traits;
+	};
+
+	PatternMatrix *pm;
+	Matrix<double> * traits;
+
+};
+
+#endif /* TYPE_LATENTTRAITS_H_ */
