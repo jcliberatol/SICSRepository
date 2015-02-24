b2ae9e46d9d2297426075640527293a7b70991e6
Yeison Garcia <yedgarciago@gmail.com>
2015-02-24 01:16:47 -0500

    initiation of the interface with R

---
+++ SICS/src/interface.h
@@ -0,0 +1,18 @@
+#ifndef _INTERFACE_H
+#include <iostream>
+#include <type/Matrix.h>
+#include <boost/dynamic_bitset.hpp>
+#include <type/PatternMatrix.h>
+#include <model/Model.h>
+#include <model/ModelFactory.h>
+#include <model/SICSGeneralModel.h>
+#include <estimation/classical/EMEstimation.h>
+#include <input/Input.h>
+#include <time.h>
+#include <trace/Trace.h>
+
+
+#define _INTERFACE_H
+	void estimatingParameters(int **, int, int, char *, int , char *, double, int, bool);
+	void profilerOut(Trace*, int );
+#endif
