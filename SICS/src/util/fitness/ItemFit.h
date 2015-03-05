52e20be637021a36ee9829ecd73cbc690e671611
anmrdz <anmrdz@gmail.com>
2015-03-05 14:40:43 -0500

    completing itemFit

--- SICS/src/util/fitness/ItemFit.h
+++ SICS/src/util/fitness/ItemFit.h
@@ -61,31 +61,41 @@ void itemFit(LatentTraits* scores, PatternMatrix* data,Model* model ){
 				c = parameterSet[2][0][i];
 				break;
 			}
-			//TODO check qlogis function
+
 			cp = log(c/(1-c));
 			P[j][i] = 1 / (exp(cp)/(1+exp(cp))+ (1-(exp(cp)/(1+exp(cp))))*(1 + exp(-Constant::D_CONST*(a*scoresTot[j]+ b))));
 			Q[j][i] = 1 - P[j][i];
 		}
 	}
 
-//	LL[data == 1] = P[data == 1]
-//	  LL[data == 0] = Q[data == 0]
-//	  LL = colSums(log(LL))
-//
-//
-//	  mu = sigmaCuad = rep(0,nitems)
-//	  for( i in 1:nitems){
-//	    Pi = cbind(P[,i],Q[,i])
-//	    logPi = log(Pi)
-//	    mu[i] = sum(Pi * logPi)
-//	    #sigmaCuad = sigmaCuad + Pi[,1] * Pi[,2] * (log(Pi[,1]/Pi[,2])^2)
-//	    sigmaCuad[i] = sum(Pi[,1] * Pi[,2] * (log(Pi[,1]/Pi[,2])^2))
-//
-//	  }
-//	  print("mu")
-//	  print(mu)
-//	  print(sigmaCuad)
-//	  Z3 = (LL - mu) / sqrt(sigmaCuad)
+	for(i = 0; i<nitems; i++){
+		for(j = 0; j<ninds; j++){
+			if(data->bitset_list[j][i] == 1){
+				LL[j][i] = P[j][i];
+			}else{
+				LL[j][i] = Q[j][i];
+			}
+		}
+	}
+
+	double sum = 0;
+	for(j = 0; j < nitems; j++){
+		sum = 0;
+		for(i = 0; i < ninds ; i++){
+			sum += log(LL[i][j]);
+		}
+	}
+
+	double sigmaCuad[nitems] = {0}, mu[nitems] = {0};
+
+	for(i = 0; i < nitems; i++){
+		  for(j = 0; j < ninds; j++){
+			  mu[i] += log(P[j][i])* P[j][i] + log(Q[j][i])* Q[j][i];
+			  sigmaCuad[i] += P[j][1] * P[j][2] *( log(P[j][1]/P[j][2])* log(P[j][1]/P[j][2])) + Q[j][1] * Q[j][2] * (log(Q[j][1]/Q[j][2])*log(Q[j][1]/Q[j][2]));
+		  }
+	}
+
+	//	  Z3 = (LL - mu) / sqrt(sigmaCuad)
 
 }
 
