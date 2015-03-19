4755f50c419bba0875834f67cb6542630b30bf7c
akumahikki <cesandovalp@unal.edu.co>
2015-03-19 16:49:45 +0000

    Update for test optimizer

--- SICS/src/estimation/bayesian/LatentTraitEstimation.h
+++ SICS/src/estimation/bayesian/LatentTraitEstimation.h
@@ -12,14 +12,28 @@
 #include <sstream>
 #include <type/Constant.h>
 #include <optimizer/Brent_fmin.h>
+#include <cstdlib>
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_min.h>
+
 #define t_zita t_model->parameterModel->parameterSet
 #define gg t_model->parameterModel->successProbability
 
 class LatentTraitEstimation {
 public:
+
+	struct parameters_logL {
+		bool * pattern;
+		int size;
+		int node;
+		Model *model;
+	};
+
 	LatentTraitEstimation() {
 
 	}
+
 	virtual ~LatentTraitEstimation() {
 	}
 
@@ -62,7 +76,7 @@ public:
 		for (int i = 0; i < pattern.size(); i++) {
 			if (pattern.at(i) > 0) {
 				p *= gg(theta, new double[3] { t_zita[0][0][i], t_zita[1][0][i],
-						t_zita[2][0][i] });
+				t_zita[2][0][i] });
 
 			} else {
 				p *= 1 - gg(theta, new double[3] { t_zita[0][0][i],
@@ -79,7 +93,7 @@ public:
 		for (int i = 0; i < size; i++) {
 			if (pattern[i] > 0) {
 				p *= gg(theta, new double[3] { t_zita[0][0][i], t_zita[1][0][i],
-						t_zita[2][0][i] });
+				t_zita[2][0][i] });
 
 			} else {
 				p *= 1 - gg(theta, new double[3] { t_zita[0][0][i],
@@ -129,6 +143,7 @@ public:
 
 		bool ** pattern_list = lt->pm->getBitsetList();
 		int size = lt->pm->matrix.size();
+
 		int counter = 0;
 
 		for (int index = 0; index < size; index++, ++counter) {
@@ -146,11 +161,11 @@ public:
 			(*lt->traits)(counter, lt->dim - 1) = sum_num / sum_den;
 		}
 
-//		for (int j = 0; j < size; j++) {
-//			delete pattern_list[j];
-//		}
+		for (int j = 0; j < size; j++) {
+			delete pattern_list[j];
+		}
 
-		//delete pattern_list;
+		delete pattern_list;
 	}
 
 	//Deprecated
@@ -165,10 +180,49 @@ public:
 			Model *model) {
 		return (-(log(patternProbabilities(theta, pattern, size, node, model))
 				- ((theta * theta) / 2)));
+	}
+
+	static double logLR(double theta, void * params) {
+		parameters_logL * temp = (struct parameters_logL*) params;
+
+		return (-(log(
+				patternProbabilities(theta, temp->pattern, temp->size,
+						temp->node, temp->model)) - ((theta * theta) / 2)));
+	}
+
+	void printVectors() {
+		bool ** pattern_list = lt->pm->getBitsetList();
+		int * frequency_list = lt->pm->getFrequencyList();
+		int size = lt->pm->matrix.size();
+
+		int counter = 0;
+
+//		for (int index = 0; index < size; index++, ++counter) {
+//			if (rand() < 214748364.7) {
+				//cout << "theta, function" << index << endl;
+				for (double i = -3.3; i <= 2.8; i += .01) {
+					cout << i << ","
+							<< logL(i, pattern_list[0], lt->pm->size,
+									counter, this->model) << endl;
+				}
+//			}
+//		}
+	}
+
+	void evaluate_theta(double theta, int pattern) {
+		bool ** pattern_list = lt->pm->getBitsetList();
+		int * frequency_list = lt->pm->getFrequencyList();
+		int size = lt->pm->matrix.size();
+
+		int counter = 0;
+
+		cout << theta << ","
+				<< logL(theta, pattern_list[pattern], lt->pm->size, counter,
+						this->model) << endl;
 
 	}
 
-	//Deprecated
+//Deprecated
 	void estimateLatentTraitsMAP_() {
 		map<vector<char>, int>::const_iterator it;
 		map<vector<char>, int>::const_iterator begin = lt->pm->matrix.begin();
@@ -196,14 +250,108 @@ public:
 			(*lt->traits)(counter, lt->dim - 1) = Brent_fmin(new double[2] { -5,
 					5 }, 0.0001220703, function, pattern_list[index],
 					lt->pm->size, counter, this->model, 1);
+//			for(double i = -0.3; i <= 0.3; i+=.005){
+//				//cout<<"index: "<<index<<", i: "<<i<<endl;
+//				double first = logL((*lt->traits)(counter, lt->dim - 1)+i, pattern_list[index], lt->pm->size, counter, this->model);
+//				double second = logL((*lt->traits)(counter, lt->dim - 1), pattern_list[index], lt->pm->size, counter, this->model);
+//				cout<<"f("<<(*lt->traits)(counter, lt->dim - 1)+i<<")"<<"<"<<"f("<<(*lt->traits)(counter, lt->dim - 1)<<")"<<endl;
+//				cout<<first<<"<"<<second;
+//				if(first < second){
+//					cout<<"→Menor";
+//					(*lt->traits)(counter, lt->dim - 1) = (*lt->traits)(counter, lt->dim - 1)+i;
+//				}
+//				cout<<endl;
+//			}
+//			cout<<"----------------I'm a separator bar.-----------------"<<endl;
 		}
 
-//		for (int j = 0; j < size; j++) {
-//			delete pattern_list[j];
-//		}
+		for (int j = 0; j < size; j++) {
+			delete pattern_list[j];
+		}
 
-		//delete pattern_list;
-		//delete frequency_list;
+		delete pattern_list;
+		delete frequency_list;
+	}
+
+	void estimateLatentTraitsMAP_R() {
+		bool ** pattern_list = lt->pm->getBitsetList();
+		int * frequency_list = lt->pm->getFrequencyList();
+		int size = lt->pm->matrix.size();
+		parameters_logL temp;
+
+		int counter = 0;
+
+		for (int index = 0; index < size; index++, ++counter) {
+			temp.model = model;
+			temp.node = counter;
+			temp.pattern = pattern_list[index];
+			temp.size = lt->pm->size;
+			double (*function)(double, void*) = &logLR;
+			(*lt->traits)(counter, lt->dim - 1) = Brent_fmin(-5, 5, function,
+					(void*) &temp, 0.0001220703);
+		}
+
+		for (int j = 0; j < size; j++) {
+			delete pattern_list[j];
+		}
+
+		delete pattern_list;
+		delete frequency_list;
+	}
+
+	void estimateLatentTraitsMAP_GSL() {
+		int status;
+		int iter = 0, max_iter = 100;
+		const gsl_min_fminimizer_type *T;
+		parameters_logL temp;
+		gsl_min_fminimizer *s;
+		gsl_function F;
+		F.function = &logLR;
+		F.params = &temp;
+
+		T = gsl_min_fminimizer_quad_golden;
+		s = gsl_min_fminimizer_alloc(T);
+
+		double m = 2.0, m_expected = M_PI;
+		double a = -5.0, b = 5.0;
+
+		gsl_min_fminimizer_set(s, &F, m, a, b);
+
+		bool ** pattern_list = lt->pm->getBitsetList();
+		int * frequency_list = lt->pm->getFrequencyList();
+		int size = lt->pm->matrix.size();
+
+		int counter = 0;
+
+		for (int index = 0; index < size; index++, ++counter) {
+			cout << "." << endl;
+			temp.model = model;
+			temp.node = counter;
+			temp.pattern = pattern_list[index];
+			temp.size = lt->pm->size;
+
+			F.params = (void*) &temp;
+
+			do {
+				status = gsl_min_fminimizer_iterate(s);
+
+				m = gsl_min_fminimizer_x_minimum(s);
+				a = gsl_min_fminimizer_x_lower(s);
+				b = gsl_min_fminimizer_x_upper(s);
+
+				status = gsl_min_test_interval(a, b, 0.001, 0.0);
+
+			} while (status == GSL_CONTINUE && iter < max_iter);
+
+			(*lt->traits)(counter, lt->dim - 1) = m;
+		}
+
+		for (int j = 0; j < size; j++) {
+			delete pattern_list[j];
+		}
+
+		delete pattern_list;
+		delete frequency_list;
 	}
 
 	void setQuadratureNodes(QuadratureNodes *nodes) {
