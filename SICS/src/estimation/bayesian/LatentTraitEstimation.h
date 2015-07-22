f2d3b4876d8c8f66e8d3828c52db62613b6eeebe
akumahikki <cesandovalp@unal.edu.co>
2015-07-22 11:26:25 -0500

    Fixed bad memory management - The stupid error of the day

--- SICS/src/estimation/bayesian/LatentTraitEstimation.h
+++ SICS/src/estimation/bayesian/LatentTraitEstimation.h
@@ -60,12 +60,20 @@ public:
     static double probabilities(double theta, bool * pattern, int size, int node, Model * t_model)
     {
         double p = 1;
+        double temp_zita[3];
 
         for (int i = 0; i < size; i++)
+        {
+            temp_zita[0] = t_zita[_A];
+            temp_zita[1] = t_zita[_B];
+            temp_zita[2] = t_zita[_C];
+
             if (pattern[i] > 0)
-                p *= gg(theta, new double[3] { t_zita[_A], t_zita[_B], t_zita[_C] });
+                p *= gg(theta, temp_zita);
             else
-                p *= 1 - gg(theta, new double[3] { t_zita[_A], t_zita[_B], t_zita[_C] });
+
+                p *= 1 - gg(theta, temp_zita);
+        }
 
         return (p);
     }
@@ -73,12 +81,19 @@ public:
     static double probabilities(double theta, bool * pattern, int size, int node, Model * t_model, double *** parSet)
     {
         double p = 1;
+        double temp_zita[3];
 
         for (int i = 0; i < size; i++)
+        {
+            temp_zita[0] = parSet[_A];
+            temp_zita[1] = parSet[_B];
+            temp_zita[2] = parSet[_C];
+
             if (pattern[i] > 0)
-                p *= gg(theta, new double[3] { parSet[_A], parSet[_B], parSet[_C] });
+                p *= gg(theta, temp_zita);
             else
-                p *= 1 - gg(theta, new double[3] { parSet[_A], parSet[_B], parSet[_C] });
+                p *= 1 - gg(theta, temp_zita);
+        }
 
         return (p);
     }
@@ -91,20 +106,21 @@ public:
 
     void estimateLatentTraitsEAP(double *** parSet)
     {
-	model->parameterModel->transform();
+        model->parameterModel->transform();
         bool ** list = lt->pm->getBitsetList();
         int size = lt->pm->matrix.size();
 
-        int counter = 0;
+        int counter = 0, i;
+        double sum_num, sum_den, pp;
 
         for (int pattern = 0; pattern < size; pattern++, ++counter)
         {
-            double sum_num = 0;
-            double sum_den = 0;
+            sum_num = 0;
+            sum_den = 0;
 
-            for (int i = 0; i < quadNodes->size(); ++i)
+            for (i = 0; i < quadNodes->size(); ++i)
             {
-                double pp = probabilities((*quadNodes->getTheta())(0, i), list[pattern], lt->pm->size, i, this->model, parSet);
+                pp = probabilities((*quadNodes->getTheta())(0, i), list[pattern], lt->pm->size, i, this->model, parSet);
 
                 sum_num += (*quadNodes->getTheta())(0, i) * ((*quadNodes->getWeight())(0, i)) * pp;
                 sum_den += (*quadNodes->getWeight())(0, i) * pp;
@@ -112,25 +128,26 @@ public:
 
             (*lt->traits)(counter, lt->dim - 1) = sum_num / sum_den;
         }
-	model->parameterModel->untransform();
+        model->parameterModel->untransform();
     }
 
     void estimateLatentTraitsEAP()
     {
-	model->parameterModel->transform();
+        model->parameterModel->transform();
         bool ** list = lt->pm->getBitsetList();
         int size = lt->pm->matrix.size();
 
-        int counter = 0;
+        int counter = 0, i;
+        double sum_num, sum_den, pp;
 
         for (int pattern = 0; pattern < size; pattern++, ++counter)
         {
-            double sum_num = 0;
-            double sum_den = 0;
+            sum_num = 0;
+            sum_den = 0;
 
-            for (int i = 0; i < quadNodes->size(); ++i)
+            for (i = 0; i < quadNodes->size(); ++i)
             {
-                double pp = probabilities(list[pattern], lt->pm->size, i);
+                pp = probabilities(list[pattern], lt->pm->size, i);
 
                 sum_num += (*quadNodes->getTheta())(0, i) * ((*quadNodes->getWeight())(0, i)) * pp;
                 sum_den += (*quadNodes->getWeight())(0, i) * pp;
@@ -138,7 +155,7 @@ public:
 
             (*lt->traits)(counter, lt->dim - 1) = sum_num / sum_den;
         }
-	model->parameterModel->untransform();
+        model->parameterModel->untransform();
     }
 
     static double logL(double theta, bool * pattern, int size, int node, Model *model)
@@ -174,8 +191,8 @@ public:
 
     void estimateLatentTraitsMAP()
     {
-	double BOUNDS[] = {-5,5};
-	model->parameterModel->transform();
+        model->parameterModel->transform();
+        double BOUNDS[] = {-5,5};
         bool ** pattern_list = lt->pm->getBitsetList();
         int size = lt->pm->matrix.size();
 
@@ -184,13 +201,14 @@ public:
         for (int index = 0; index < size; index++, ++counter)
             (*lt->traits)(counter, lt->dim - 1) =
             Brent_fmin(BOUNDS, _DELTA, &logL, pattern_list[index], lt->pm->size, counter, this->model, 1);
-	model->parameterModel->untransform();
+        
+        model->parameterModel->untransform();
     }
 
     void estimateLatentTraitsMAP(double *** parSet)
     {
-	double BOUNDS[] = {-5,5};
-	model->parameterModel->transform();
+        model->parameterModel->transform();
+        double BOUNDS[] = {-5,5};
         bool ** pattern_list = lt->pm->getBitsetList();
         int size = lt->pm->matrix.size();
 
@@ -199,7 +217,7 @@ public:
         for (int index = 0; index < size; index++, ++counter)
             (*lt->traits)(counter, lt->dim - 1) = Brent_fmin(BOUNDS, _DELTA, &logLP, pattern_list[index],
                 lt->pm->size, counter, this->model, parSet, 1);
-	model->parameterModel->untransform();
+        model->parameterModel->untransform();
     }
 
     void estimateLatentTraitsMAP_R()
