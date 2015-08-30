4a736991594c9cc1d7ed0251b60f3d590cc23080
akumabps <cesandovalp@unal.edu.co>
2015-08-30 11:47:03 -0500

    Update LatentTraits memory management

--- SICS/src/type/LatentTraits.h
+++ SICS/src/type/LatentTraits.h
@@ -14,57 +14,64 @@ class LatentTraits
 
 public:
 
-	int dim;
+    int dim;
 
-	LatentTraits(PatternMatrix * p, const int dims = 1)
-	{
-		int rows;
-		
-		pm = p;
-		rows = pm->matrix.size();
-		traits = new Matrix<double>(rows, dims);
-		dim = dims;
-	}
+    LatentTraits(PatternMatrix * p, const int dims = 1)
+    {
+        int rows;
+        
+        pm = p;
+        rows = pm->matrix.size();
+        traits = new Matrix<double>(rows, dims);
+        dim = dims;
+    }
 
-	double ** getListPatternTheta()
-	{
-		double ** result;
-		bool ** pattern_list;
-		int items;
+    double ** getListPatternTheta()
+    {
+        double ** result;
+        bool ** pattern_list;
+        int items;
 
-		pattern_list = pm->getBitsetList();
-		result = new double*[pm->matrix.size()];
-		items = pm->countItems();
+        pattern_list = pm->getBitsetList();
+        result = new double*[pm->matrix.size()];
+        items = pm->countItems();
 
-		for(unsigned int i = 0; i < pm->matrix.size(); i++)
-		{
-			result[i] = new double[items + 1];
+        for(unsigned int i = 0; i < pm->matrix.size(); i++)
+        {
+            result[i] = new double[items + 1];
 
-			for(int j = 0; j < items; j++)
-				result[i][j] = pattern_list[i][j];
-			
-			result[i][items] = (*traits)(i,0);
-		}
+            for(int j = 0; j < items; j++)
+                result[i][j] = pattern_list[i][j];
+            
+            result[i][items] = (*traits)(i,0);
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	virtual ~LatentTraits() { delete traits; };
+    void deleteListPatternTheta(double ** p)
+    {
+        for(int i = 0; i < 10; i++) delete [] p[i];
+        delete [] p;
 
-	void print()
-	{
-		bool ** pattern_list = pm->getBitsetList();
+    }
 
-		for(unsigned int i = 0; i < pm->matrix.size(); i++)
-		{
-			for(int j = 0; j < pm->countItems(); j++)
-				cout << pattern_list[i][j] << " ";
-			cout<<(*traits)(i,0)<<endl;
-		}
-	}
+    virtual ~LatentTraits() { delete traits; };
 
-	PatternMatrix *pm;
-	Matrix<double> * traits;
+    void print()
+    {
+        bool ** pattern_list = pm->getBitsetList();
+
+        for(unsigned int i = 0; i < pm->matrix.size(); i++)
+        {
+            for(int j = 0; j < pm->countItems(); j++)
+                cout << pattern_list[i][j] << " ";
+            cout<<(*traits)(i,0)<<endl;
+        }
+    }
+
+    PatternMatrix *pm;
+    Matrix<double> * traits;
 };
 
 #endif /* TYPE_LATENTTRAITS_H_ */
