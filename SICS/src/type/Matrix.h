4bc5c7fe704ede0e3210ab0974d2ea2496674d39
akumabps <cesandovalp@unal.edu.co>
2015-09-07 18:56:41 -0500

    Improved matrix class

--- SICS/src/type/Matrix.h
+++ SICS/src/type/Matrix.h
@@ -40,32 +40,32 @@ ostream& operator<<(ostream &, Matrix<T> &);
 template<class T>
 class Matrix {
 private:
-	int nCol;
-	int nRow;
+	unsigned int nCol;
+	unsigned int nRow;
 	T m(char);
 	T get3x3determinant ();
 
 public:
 	//bool transposed;
 	bool symmetric;
-	T *memory;
-	int ld;
+	T **memory;
+	unsigned int ld;
 	static char del;
-	Matrix(T**, int, int);/*Importing a 2D array*/
+	Matrix(T**, unsigned int, unsigned int);/*Importing a 2D array*/
 	Matrix(); /**Empty object*/
-	Matrix(int, int); /**Two dimensional Matrix Constructor allocates memory*/
+	Matrix(unsigned int, unsigned int); /**Two dimensional Matrix Constructor allocates memory*/
 	Matrix(Matrix<T>&); /**Copy constructor*/
-	Matrix(char I, int size); /**Create special kinds of matrices (dense identity)*/
+	Matrix(char I, unsigned int size); /**Create special kinds of matrices (dense identity)*/
 	void reset();/**Reset method, puts all entries in zeros*/
 	void transpose ();/**Transposes the matrix, notice it does not perform memory transpose, only index transpose*/
 	void copy(Matrix<T>&);/**Copy constructor*/
-	int nR(); /** Returns number of rows */
-	int nC(); /** Returns number of columns */
-	void setIndex(int, int, T);
-	T getIndex(int, int);
+	unsigned int nR(); /** Returns number of rows */
+	unsigned int nC(); /** Returns number of columns */
+	void setIndex(unsigned int, unsigned int, T);
+	T getIndex(unsigned int, unsigned int);
 	T sum(); /** Returns the sum of all objects */
-	inline T & operator()(const int nCol, const int nRow); /** Accessing operator for a element */
-	T & operator()(const int element); /**Accessing operator for a element */
+	inline T & operator()(const unsigned int nCol, const unsigned int nRow); /** Accessing operator for a element */
+	T & operator()(const unsigned int element); /**Accessing operator for a element */
 	friend ostream& operator<<<T>(ostream &, Matrix<T> &); /** Output operator */
 	bool isSymmetric() const; /** Symmetry flag for optimizations */
 	void setSymmetric(bool symmetric); /** Set to true the symmetry flag */
@@ -76,40 +76,30 @@ template<class T>
 char Matrix<T>::del = ' ';
 
 template<class T>
-T Matrix<T>::m(char c){
-	return (memory[c - 'a']);
+T Matrix<T>::sum()
+{
+  T sum = 0;
+
+  for (unsigned int i = 0; i < nRow; i++)
+    for (unsigned int j = 0; j < nCol; j++)
+      sum += memory[i];
+
+  return (sum);
 }
 
 template<class T>
-T Matrix<T>::sum() {
-	T sum = 0;
-
-	for (int i = 0; i < nRow * nCol; i++) {
-		sum += memory[i];
-	}
-
-	return (sum);
-}
+unsigned int Matrix<T>::nR() { return (nRow); }
 
 template<class T>
-int Matrix<T>::nR() {
-	return (nRow);
-}
+unsigned int Matrix<T>::nC() { return (nCol); }
 
 template<class T>
-int Matrix<T>::nC() {
-	return (nCol);
-}
+inline void Matrix<T>::setIndex(unsigned int r, unsigned int c, T value)
+{ memory[r][c] = value; }
 
 template<class T>
-inline void Matrix<T>::setIndex(int r, int c, T value) {
-		memory[nCol * r + c] = value;
-}
-
-template<class T>
-inline T Matrix<T>::getIndex(int r, int c) {
-		return memory[nCol * r + c];
-}
+inline T Matrix<T>::getIndex(unsigned int r, unsigned int c)
+{ return memory[r][c]; }
 
 template<class T>
 Matrix<T>::Matrix() {
@@ -128,62 +118,40 @@ Matrix<T>::Matrix(Matrix<T>& a) {
 }
 
 template<class T>
-Matrix<T>::Matrix(int r, int c) {
+Matrix<T>::Matrix(unsigned int r, unsigned int c) {
 	nCol = c;
 	nRow = r;
 	//transposed = false;
-	memory = new T[c * r];
+	memory = new T*[r];
+	for(unsigned int i = 0; i < r; i++)
+	  memory[i] = new T[c];
+	
 	symmetric = false;
 	ld = c;
 }
 
 template<class T>
-Matrix<T>::Matrix(T** mem , int r, int c) {
+Matrix<T>::Matrix(T** mem , unsigned int r, unsigned int c) {
 	nCol = c;
 	nRow = r;
 	//transposed = false;
-	memory = new T[c * r];
+	memory = new T*[r];
+	for(unsigned int i = 0; i < r; i++)
+	  memory[i] = new T[c];
 	symmetric = false;
 	ld = c;
 
-	for (int var = 0; var < r; ++var) {
-		for (int j = 0; j < c; ++j) {
-			memory[nCol * var + j] = mem[var][j];
+	for (unsigned int var = 0; var < r; ++var) {
+		for (unsigned int j = 0; j < c; ++j) {
+			memory[var][j] = mem[var][j];
 		}
 	}
 }
 
 template<class T>
-Matrix<T>::Matrix(char I, int c) {
-	nCol = c;
-	nRow = c;
-	//transposed = false;
-	memory = new T[c * c];
-	ld = c;
-	symmetric = false;
-	this->reset();
-	if(I=='I'){
-		for(int i = 0 ; i < c ; i++){
-			(*this)(i,i)=static_cast<T>(1);
-		}
-	}
-	if(I=='R'){
-		srand(time(NULL));
-		for(int i = 0 ; i < c*c ; i++){
-			memory[i] = (T)(rand())/(T)(INT_MAX);
-		}
-	}
-}
+inline T & Matrix<T>::operator()(const unsigned int r, const unsigned int c) {
+		return (memory[r][c]);
 
-template<class T>
-inline T & Matrix<T>::operator()(const int r, const int c) {
-		return (memory[nCol * r + c]);
-
-}
-
-template<class T>
-T & Matrix<T>::operator()(const int el) {
-		return (memory[el]);
 }
 
 template<class T>
@@ -197,7 +165,12 @@ inline void Matrix<T>::setSymmetric(bool symmetric) {
 }
 
 template<class T>
-Matrix<T>::~Matrix() { delete[] memory; }
+Matrix<T>::~Matrix()
+{
+  for(unsigned int i = 0; i < nRow; i++)
+    delete memory[i];
+  delete[] memory;
+}
 
 template<class T>
 void Matrix<T>::reset() {
@@ -208,7 +181,9 @@ template<class T>
 void Matrix<T>::copy(Matrix<T>& a) {
 	this->nCol = a.nC();
 	this->nRow = a.nR();
-	memory = new T[a.nC() * a.nR()];
+	memory = new T*[a.nR()];
+	for(unsigned int i = 0; i < a.nR(); i++)
+	  memory[i] = new T[a.nC()];
 
 	memcpy(memory,a.memory,sizeof(T)*a.nC()*a.nR());
 	symmetric = a.symmetric;
@@ -218,8 +193,8 @@ template<class T>
 ostream& operator<<(ostream &out, Matrix<T> &M) {
 	// Since operator<< is a friend of the Point class, we can access
 	// Point's members directly.
-	for (int i = 0; i < M.nR(); ++i) {
-		for (int j = 0; j < M.nC(); j++) {
+	for (unsigned int i = 0; i < M.nR(); ++i) {
+		for (unsigned int j = 0; j < M.nC(); j++) {
 			out << M(i, j) << M.del;
 		}
 		out << endl;
