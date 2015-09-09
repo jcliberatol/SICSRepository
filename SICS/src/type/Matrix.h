42bf3f105d35d3dbb445fd765c328f35cb94240b
akumabps <cesandovalp@unal.edu.co>
2015-09-09 11:38:22 -0500

    Update again

--- SICS/src/type/Matrix.h
+++ SICS/src/type/Matrix.h
@@ -38,53 +38,56 @@ template<typename T>
 ostream& operator<<(ostream &, Matrix<T> &);
 
 template<class T>
-class Matrix {
+class Matrix
+{
 private:
-	unsigned int nCol;
-	unsigned int nRow;
-	T m(char);
-	T get3x3determinant ();
+    unsigned int nCol;
+    unsigned int nRow;
+    T m(char);
+    T get3x3determinant ();
 
 public:
-	//bool transposed;
-	bool symmetric;
-	T **memory;
-	unsigned int ld;
-	static char del;
-	Matrix(T**, unsigned int, unsigned int);/*Importing a 2D array*/
-	Matrix(); /**Empty object*/
-	Matrix(unsigned int, unsigned int); /**Two dimensional Matrix Constructor allocates memory*/
-	Matrix(Matrix<T>&); /**Copy constructor*/
-	Matrix(char I, unsigned int size); /**Create special kinds of matrices (dense identity)*/
-	void reset();/**Reset method, puts all entries in zeros*/
-	void transpose ();/**Transposes the matrix, notice it does not perform memory transpose, only index transpose*/
-	void copy(Matrix<T>&);/**Copy constructor*/
-	unsigned int nR(); /** Returns number of rows */
-	unsigned int nC(); /** Returns number of columns */
-	void setIndex(unsigned int, unsigned int, T);
-	T getIndex(unsigned int, unsigned int);
-	T sum(); /** Returns the sum of all objects */
-	inline T & operator()(const unsigned int nCol, const unsigned int nRow); /** Accessing operator for a element */
-	T & operator()(const unsigned int element); /**Accessing operator for a element */
-	friend ostream& operator<<<T>(ostream &, Matrix<T> &); /** Output operator */
-	bool isSymmetric() const; /** Symmetry flag for optimizations */
-	void setSymmetric(bool symmetric); /** Set to true the symmetry flag */
-	virtual ~Matrix();
+    //bool transposed;
+    bool symmetric;
+    T *memory;
+    unsigned int ld;
+    static char del;
+    Matrix(T**, unsigned int, unsigned int);/*Importing a 2D array*/
+    Matrix(); /**Empty object*/
+    Matrix(unsigned int, unsigned int); /**Two dimensional Matrix Constructor allocates memory*/
+    Matrix(Matrix<T>&); /**Copy constructor*/
+    Matrix(char I, unsigned int size); /**Create special kinds of matrices (dense identity)*/
+    void reset();/**Reset method, puts all entries in zeros*/
+    void transpose ();/**Transposes the matrix, notice it does not perform memory transpose, only index transpose*/
+    void copy(Matrix<T>&);/**Copy constructor*/
+    unsigned int nR(); /** Returns number of rows */
+    unsigned int nC(); /** Returns number of columns */
+    void setIndex(unsigned int, unsigned int, T);
+    T getIndex(unsigned int, unsigned int);
+    T sum(); /** Returns the sum of all objects */
+    inline T & operator()(const unsigned int nCol, const unsigned int nRow); /** Accessing operator for a element */
+    T & operator()(const unsigned int element); /**Accessing operator for a element */
+    friend ostream& operator<<<T>(ostream &, Matrix<T> &); /** Output operator */
+    bool isSymmetric() const; /** Symmetry flag for optimizations */
+    void setSymmetric(bool symmetric); /** Set to true the symmetry flag */
+    virtual ~Matrix();
 };
 
 template<class T>
 char Matrix<T>::del = ' ';
 
 template<class T>
+T Matrix<T>::m(char c) { return (memory[c - 'a']); }
+
+template<class T>
 T Matrix<T>::sum()
 {
-  T sum = 0;
+    T sum = 0;
 
-  for (unsigned int i = 0; i < nRow; i++)
-    for (unsigned int j = 0; j < nCol; j++)
-      sum += memory[i];
+    for (unsigned int i = 0; i < nRow * nCol; i++)
+        sum += memory[i];
 
-  return (sum);
+    return (sum);
 }
 
 template<class T>
@@ -95,111 +98,125 @@ unsigned int Matrix<T>::nC() { return (nCol); }
 
 template<class T>
 inline void Matrix<T>::setIndex(unsigned int r, unsigned int c, T value)
-{ memory[r][c] = value; }
+{ memory[nCol * r + c] = value; }
 
 template<class T>
 inline T Matrix<T>::getIndex(unsigned int r, unsigned int c)
-{ return memory[r][c]; }
+{ return memory[nCol * r + c]; }
 
 template<class T>
-Matrix<T>::Matrix() {
-	nCol = 0;
-	nRow = 0;
-	ld = 0;
-	memory = NULL;
-	//transposed = false;
-	symmetric = false;
-}
-
-template<class T>
-Matrix<T>::Matrix(Matrix<T>& a) {
-	memory = NULL;
-	copy(a);
-}
-
-template<class T>
-Matrix<T>::Matrix(unsigned int r, unsigned int c) {
-	nCol = c;
-	nRow = r;
-	//transposed = false;
-	memory = new T*[r];
-	for(unsigned int i = 0; i < r; i++)
-	  memory[i] = new T[c];
-	
-	symmetric = false;
-	ld = c;
-}
-
-template<class T>
-Matrix<T>::Matrix(T** mem , unsigned int r, unsigned int c) {
-	nCol = c;
-	nRow = r;
-	//transposed = false;
-	memory = new T*[r];
-	for(unsigned int i = 0; i < r; i++)
-	  memory[i] = new T[c];
-	symmetric = false;
-	ld = c;
-
-	for (unsigned int var = 0; var < r; ++var) {
-		for (unsigned int j = 0; j < c; ++j) {
-			memory[var][j] = mem[var][j];
-		}
-	}
-}
-
-template<class T>
-inline T & Matrix<T>::operator()(const unsigned int r, const unsigned int c) {
-		return (memory[r][c]);
-
-}
-
-template<class T>
-inline bool Matrix<T>::isSymmetric() const {
-	return (symmetric);
-}
-
-template<class T>
-inline void Matrix<T>::setSymmetric(bool symmetric) {
-	this->symmetric = symmetric;
-}
-
-template<class T>
-Matrix<T>::~Matrix()
+Matrix<T>::Matrix()
 {
-  for(unsigned int i = 0; i < nRow; i++)
-    delete memory[i];
-  delete[] memory;
+    nCol = 0;
+    nRow = 0;
+    ld = 0;
+    memory = NULL;
+    //transposed = false;
+    symmetric = false;
 }
 
 template<class T>
-void Matrix<T>::reset() {
-	memset(memory, 0.0, (nCol * nRow) * sizeof(T));
+Matrix<T>::Matrix(Matrix<T>& a)
+{
+    memory = NULL;
+    copy(a);
 }
 
 template<class T>
-void Matrix<T>::copy(Matrix<T>& a) {
-	this->nCol = a.nC();
-	this->nRow = a.nR();
-	memory = new T*[a.nR()];
-	for(unsigned int i = 0; i < a.nR(); i++)
-	  memory[i] = new T[a.nC()];
-
-	memcpy(memory,a.memory,sizeof(T)*a.nC()*a.nR());
-	symmetric = a.symmetric;
+Matrix<T>::Matrix(unsigned int r, unsigned int c)
+{
+    nCol = c;
+    nRow = r;
+    //transposed = false;
+    memory = new T[c * r];
+    symmetric = false;
+    ld = c;
 }
 
 template<class T>
-ostream& operator<<(ostream &out, Matrix<T> &M) {
-	// Since operator<< is a friend of the Point class, we can access
-	// Point's members directly.
-	for (unsigned int i = 0; i < M.nR(); ++i) {
-		for (unsigned int j = 0; j < M.nC(); j++) {
-			out << M(i, j) << M.del;
-		}
-		out << endl;
-	}
-	return (out);
+Matrix<T>::Matrix(T** mem , unsigned int r, unsigned int c)
+{
+    nCol = c;
+    nRow = r;
+    //transposed = false;
+    memory = new T[c * r];
+    symmetric = false;
+    ld = c;
+
+    for (unsigned int var = 0; var < r; ++var)
+        for (unsigned int j = 0; j < c; ++j)
+            memory[nCol * var + j] = mem[var][j];
+}
+
+template<class T>
+Matrix<T>::Matrix(char I, unsigned int c)
+{
+    nCol = c;
+    nRow = c;
+    //transposed = false;
+    memory = new T[c * c];
+    ld = c;
+    symmetric = false;
+    this->reset();
+    if(I=='I')
+    {
+        for(unsigned int i = 0 ; i < c ; i++)
+            (*this)(i,i)=static_cast<T>(1);
+    }
+
+    if(I=='R')
+    {
+        srand(time(NULL));
+        for(unsigned int i = 0 ; i < c*c ; i++)
+            memory[i] = (T)(rand())/(T)(INT_MAX);
+    }
+}
+
+template<class T>
+inline T & Matrix<T>::operator()(const unsigned int r, const unsigned int c)
+{ return (memory[nCol * r + c]); }
+
+template<class T>
+T & Matrix<T>::operator()(const unsigned int el) { return (memory[el]); }
+
+template<class T>
+inline bool Matrix<T>::isSymmetric() const { return (symmetric); }
+
+template<class T>
+inline void Matrix<T>::setSymmetric(bool symmetric)
+{ this->symmetric = symmetric; }
+
+template<class T>
+Matrix<T>::~Matrix() { delete[] memory; }
+
+template<class T>
+void Matrix<T>::reset()
+{ memset(memory, 0.0, (nCol * nRow) * sizeof(T)); }
+
+template<class T>
+void Matrix<T>::copy(Matrix<T>& a)
+{
+    this->nCol = a.nC();
+    this->nRow = a.nR();
+    memory = new T[a.nC() * a.nR()];
+
+    memcpy(memory,a.memory,sizeof(T)*a.nC()*a.nR());
+    symmetric = a.symmetric;
+}
+
+template<class T>
+ostream& operator<<(ostream &out, Matrix<T> &M)
+{
+    // Since operator<< is a friend of the Point class, we can access
+    // Point's members directly.
+    for (unsigned int i = 0; i < M.nR(); ++i)
+    {
+        for (unsigned int j = 0; j < M.nC(); j++)
+            out << M(i, j) << M.del;
+        out << endl;
+    }
+    
+    return (out);
 }
 
 
